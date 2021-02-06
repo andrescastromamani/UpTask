2d4f6f801ef15bc94901e829ecf1d426ddaa6875
Andres <castroandresrm@gmail.com>
2021-02-05 20:39:47 -0400

    Agregamos SweetAlert2 al formulario de crear cuenta

--- js/formulario.js
+++ js/formulario.js
@@ -1,1 +1,25 @@
-console.log('funciona');
+addEventListeners();
+function addEventListeners(){
+    document.querySelector('#formulario').addEventListener('submit', validarRegistro);
+}
+
+function validarRegistro(e){
+    e.preventDefault();
+    //console.log('aqui vamos');
+    var usuario = document.querySelector('#usuario').value,
+        password = document.querySelector('#password').value;
+
+    if(usuario === '' || password === ''){
+        Swal.fire({
+            icon: 'error',
+            title: 'Hubo un Error!',
+            text: 'Todos los campos son Obligatorios'
+          })
+    }else{
+        Swal.fire({
+            icon: 'success',
+            title: 'Correcto',
+            text: 'correctamente'
+          })
+    }
+}
