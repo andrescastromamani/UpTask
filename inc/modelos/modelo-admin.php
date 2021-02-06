b64df610b5c1cc9a2e048ffc471f8023711b313c
Andres <castroandresrm@gmail.com>
2021-02-05 21:17:17 -0400

    Encriptamos el Password

--- inc/modelos/modelo-admin.php
+++ inc/modelos/modelo-admin.php
@@ -1,2 +1,23 @@
 <?php 
- echo json_encode($_POST);
+
+    $accion = $_POST['accion'];
+    $password = $_POST['password'];
+    $usuario = $_POST['usuario'];
+
+    if($accion == 'crear'){
+        //crear los admins
+
+        //hashear el password
+        $opciones = array(
+            'cost' => 12
+        );
+        $hash_password = password_hash($password, PASSWORD_BCRYPT,$opciones);
+        $respuesta = array(
+            'pass' => $hash_password
+        );
+        echo json_encode($respuesta);
+    }
+
+    if($accion == 'login'){
+        
+    }
