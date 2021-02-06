5293acc61d5c57fea5eab716d47b80aa92044e93
Andres <castroandresrm@gmail.com>
2021-02-05 21:03:38 -0400

    Agregamos la comunicacion del Ajax con nuestro modelo

--- js/formulario.js
+++ js/formulario.js
@@ -7,19 +7,37 @@ function validarRegistro(e){
     e.preventDefault();
     //console.log('aqui vamos');
     var usuario = document.querySelector('#usuario').value,
-        password = document.querySelector('#password').value;
+        password = document.querySelector('#password').value,
+        tipo = document.querySelector('#tipo').value;
 
     if(usuario === '' || password === ''){
+        //Cuando la validacion fallo
         Swal.fire({
             icon: 'error',
             title: 'Hubo un Error!',
             text: 'Todos los campos son Obligatorios'
           })
     }else{
-        Swal.fire({
-            icon: 'success',
-            title: 'Correcto',
-            text: 'correctamente'
-          })
+        //Si los datos son correctos
+
+        //datos que se envian al servidor
+        var datos = new FormData();
+        datos.append('usuario',usuario);
+        datos.append('password',password);
+        datos.append('accion',tipo);
+        
+        //crear el llamado a Ajax
+        var xhr = new XMLHttpRequest();
+        //abrir la conexion
+        xhr.open('POST','inc/modelos/modelo-admin.php',true);
+        //retornar los datos
+        xhr.onload = function(){
+            if(this.status === 200){
+                console.log(JSON.parse(xhr.responseText));
+            }
+        }
+        //enviar los datos
+        xhr.send(datos);
+
     }
 }
