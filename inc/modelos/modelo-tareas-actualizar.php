b0c2230c0d46adda9ebaa7d3d055d9c66e84220a
Andres <castroandresrm@gmail.com>
2021-02-08 21:37:19 -0400

    Guardamos los estados de  las tareas en la BD

--- inc/modelos/modelo-tareas-actualizar.php
+++ inc/modelos/modelo-tareas-actualizar.php
@@ -1,8 +1,30 @@
 <?php
     $accion = $_POST['accion'];
     $estado = $_POST['estado'];
-
+    $id_tarea = $_POST['id'];
     if($accion == 'actualizar'){
         
-        echo json_encode($_POST);
+        //importar la conexion a l BD
+        include '../funciones/conexion.php';
+        try {
+            $stmt = $conn->prepare("UPDATE tareas set estado = ? WHERE id = ?");
+            $stmt->bind_param('ii',$estado,$id_tarea);
+            $stmt->execute();
+            if($stmt->affected_rows > 0){
+                $respuesta = array(
+                    'respuesta' => 'correcto'
+                );
+            }else{
+                $respuesta = array(
+                    'respuesta' => 'error'
+                );
+            }
+            $stmt->close();
+            $conn->close();
+        } catch (exception $e) {
+            $respuesta = array(
+                'error' => $e->getMessage()
+            );
+        }
+        echo json_encode($respuesta);
     }
